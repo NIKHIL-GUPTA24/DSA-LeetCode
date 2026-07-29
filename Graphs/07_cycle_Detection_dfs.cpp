9c34aad6f4c21fdca1fb56e2b21a8ca328613ced
NIKHIL-GUPTA24 <nikhilgupta24102006@gmail.com>
2026-07-29 23:22:12 +0530

    Add DFS cycle detection solution

---
+++ Graphs/07_cycle_Detection_dfs.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    
+    bool checkcycle(int node, vector <bool> &visited, 
+        vector<bool> &dfsvisited, vector<vector <int> >&adj){
+        visited[node] = true;
+        dfsvisited[node] = true;
+        for(auto neighbour: adj[node]){
+            if(!visited[neighbour] ){
+                bool cycledetected = checkcycle(neighbour, visited, dfsvisited, adj );
+                if(cycledetected)
+                    return true;
+            }
+            else if(dfsvisited[neighbour]){
+                return true;
+            }
+        }
+        dfsvisited[node] = false;  
+        return false;
+    }
+    
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        // code here
+        vector<vector <int> >adj(V);
+        for(int i=0;i<edges.size();i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            adj[u].push_back(v);
+        }
+        vector <bool> visited(V,false);
+        vector<bool> dfsvisited(V,false);
+        
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                bool cyclefound = checkcycle(i, visited, dfsvisited,adj);
+                if(cyclefound) return true;
+            }
+        }
+        return false;
+    }
+};
