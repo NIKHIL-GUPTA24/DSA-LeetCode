1f8e779e4a44df44e1bfabe125e596f0419ef2cc
NIKHIL-GUPTA24 <nikhilgupta24102006@gmail.com>
2026-07-31 12:34:59 +0530

    Course Schedule-1 leetcode Problem No -207

--- Graphs/11_CourseSchedule-1.cpp
+++ Graphs/11_CourseSchedule-1.cpp
@@ -1,42 +1,37 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int> > adj(numCourses);
-        vector<int>indegree (numCourses, 0);
+        vector<int> indegree(numCourses, 0);
 
         for(auto &p : prerequisites){
-            int course = p[0];  
+            int course = p[0];
             int pre = p[1];
 
             adj[pre].push_back(course);
             indegree[course]++;
         }
-        vector<int> ans;
-        queue<int>q;
+        vector<int > ans;
+        queue<int> q;
         for(int i=0;i<numCourses;i++){
             if(indegree[i] == 0){
                 q.push(i);
             }
         }
-
         while(!q.empty()){
             int node = q.front();
             q.pop();
             ans.push_back(node);
-            for(auto neighbour: adj[node]){
-                indegree[neighbour] --;
-                if(indegree[neighbour] == 0){
-                    q.push(neighbour);
+            for(auto i: adj[node]){
+                indegree[i] --;
+                if(indegree[i] == 0){
+                    q.push(i);  
                 }
             }
         }
-        if(ans.size() == numCourses){
-            return ans;
-        }
-        return{};
-        
+        if(ans.size() == numCourses ) return true;
+        return false;
     }
 };
