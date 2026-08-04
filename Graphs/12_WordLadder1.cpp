9e0b7b70a2bdcb4b634524e3c0b0d1432734da14
NIKHIL-GUPTA24 <nikhilgupta24102006@gmail.com>
2026-08-04 16:47:04 +0530

    WordLadder-1 Leetcode Problem No -127

---
+++ Graphs/12_WordLadder1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        queue<pair<string,int> >q;
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        q.push({beginWord,1});
+        st.erase(beginWord);
+
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            string word = p.first;
+            int steps = p.second;
+            
+            if(word == endWord){
+                return steps;
+            }
+            for(int i=0;i<word.length();i++){
+                char original = word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i] = ch;
+                    if(st.find(word) != st.end() ){
+                        q.emplace(word , steps +1);
+                        st.erase(word);
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        return 0;
+    }
+};
